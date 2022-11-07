92b9fe1eca49efb6c0c2fee2d72d0441f93f0d64
moretza rezaeian <morteza78rezaeian@gmail.com>
2022-11-07 10:39:08 +0330

    #adding Yup to Formik

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
+
 import './App.css';
-import SignUp from './components/SignUp';
+import SignupForm from './components/SignUp';
 
 function App() {
   return (
     <div className='App'>
-      <SignUp />
+      <SignupForm />
     </div>
   );
 }
